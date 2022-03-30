801d4d06ead848ed05b85dcf84938066979b2377
lqewo <vrkonosha@gmail.com>
2022-03-30 20:18:45 +0300

    added 5 page

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -15,7 +15,7 @@ export const otfToTtf = () => {
 		.pipe(fonter({
 			formats: ['ttf']
 		}))
-		// выгружаем в исходную папку 
+		// выгружаем в исходную папку
 		.pipe(app.gulp.dest(`${app.path.srcFolder}/fonts/`))
 }
 
@@ -58,24 +58,32 @@ export const fontsStyle = () => {
 					let fontFileName = fontsFiles[i].split('.')[0];
 					if (newFileOnly !== fontFileName) {
 						let fontName = fontFileName.split('-')[0] ? fontFileName.split('-')[0] : fontFileName;
-						let fontWeight = fontFileName.split('-')[1] ? fontFileName.split('-')[0] : fontFileName;
+						let fontWeight = fontFileName.split('-')[1] ? fontFileName.split('-')[1] : fontFileName;
 						if (fontWeight.toLowerCase() === 'thin') {
 							fontWeight = 100;
-						}	else if (fontWeight.toLowerCase() === 'extralight') {
+						}
+						else if (fontWeight.toLowerCase() === 'extralight') {
 							fontWeight = 200;
-						}	else if (fontWeight.toLowerCase() === 'light') {
+						}
+						else if (fontWeight.toLowerCase() === 'light') {
 							fontWeight = 300;
-						}	else if (fontWeight.toLowerCase() === 'medium') {
+						}
+						else if (fontWeight.toLowerCase() === 'medium') {
 							fontWeight = 500;
-						}	else if (fontWeight.toLowerCase() === 'semibold') {
+						}
+						else if (fontWeight.toLowerCase() === 'semibold') {
 							fontWeight = 600;
-						}	else if (fontWeight.toLowerCase() === 'bold') {
+						}
+						else if (fontWeight.toLowerCase() === 'bold') {
 							fontWeight = 700;
-						}	else if (fontWeight.toLowerCase() === 'extrabold' || fontWeight.toLowerCase() === 'heavy') {
+						}
+						else if (fontWeight.toLowerCase() === 'extrabold' || fontWeight.toLowerCase() === 'heavy') {
 							fontWeight = 800;
-						}	else if (fontWeight.toLowerCase() === 'black') {
+						}
+						else if (fontWeight.toLowerCase() === 'black') {
 							fontWeight = 900;
-						}	else {
+						}
+						else {
 							fontWeight = 400;
 						}
 						fs.appendFile(fontsFile, `@font-face{\n\tfont-family: ${fontName};\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`, cb);
@@ -83,7 +91,7 @@ export const fontsStyle = () => {
 					}
 				}
 			} else {
-				// Если файл есть, выводим сообщение 
+				// Если файл есть, выводим сообщение
 				console.log("Файл scss/_fonts.scss уже существует");
 			}
 		}
